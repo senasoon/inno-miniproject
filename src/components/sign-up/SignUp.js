23b8e6eb283aa46442286b68955eafafe6726283
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-09-02 20:35:32 +0900

    feat: eslint prettier적용

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const SignUp = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>;
 };
 
 export default SignUp;
--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Detail = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Detail;
