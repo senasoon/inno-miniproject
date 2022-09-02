08f60ba064a89db0dffe368e17288182152b0ad8
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-09-02 16:08:19 +0900

    feat: 세팅

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Main = () => {
+const Card = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Main = () => {
   );
 };
 
-export default Main;
+export default Card;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Main = () => {
+const Header = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Main = () => {
   );
 };
 
-export default Main;
+export default Header;
