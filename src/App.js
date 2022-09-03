e116033bb256fd13122ddefa0a33190cfc054cc4
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-09-04 00:06:23 +0900

    feat: main router 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Detail from './views/Detail';
 import Login from './views/Login';
 import Register from './views/Register';
+import Main from './views/Main';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" eletment={<Main />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/signin" element={<Register />} />
         <Route path="/login" element={<Login />} />
