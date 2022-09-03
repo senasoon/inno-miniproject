9948beb7dc1a3c3cddd253f85ac0535402213a10
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-09-04 00:07:12 +0900

    feat: delete 기능 구현

--- src/components/detail-card/Card.js
+++ src/components/detail-card/Card.js
@@ -1,16 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const Card = () => {
-  const [cardcontent, setCardContent] = useState('');
+  const param = useParams();
+  const navigate = useNavigate();
+  const [cardContent, setCardContent] = useState('');
 
   const fetchCard = async () => {
-    const { data } = await axios.get(`http://localhost:3001/post/1`);
+    const { data } = await axios.get(`http://localhost:3001/post/${param.id}`);
     setCardContent(data);
-    console.log(data);
   };
 
-  const deleteHandler = () => {};
+  const deleteHandler = () => {
+    if (
+      confirm('삭제된 데이터는 복구되지 않습니다. 게시글을 삭제 하시겠습니까?')
+    ) {
+      axios.delete(`http://localhost:3001/post/${param.id}`);
+    }
+    alert('삭제되었습니다. 홈으로 이동합니다.');
+    navigate('/');
+  };
 
   useEffect(() => {
     fetchCard();
@@ -19,7 +29,7 @@ const Card = () => {
   return (
     <div className="card">
       <div className="flex justify-between">
-        <p className="py-2">{cardcontent.nickName}</p>
+        <p className="py-2">{cardContent.nickName}</p>
         <div>
           <button className="p-2">수정</button>
           <button onClick={deleteHandler} className="py-2">
@@ -28,12 +38,12 @@ const Card = () => {
         </div>
       </div>
       <img
-        src={cardcontent.imgUrl}
+        src={cardContent.imgUrl}
         alt="닉네임이 등록한 이미지"
         className="w-[25rem]"
       />
-      <p className="font-semibold py-2">{cardcontent.title}</p>
-      <p className="font-normal pb-4 w-[25rem]">{cardcontent.content}</p>
+      <p className="font-semibold py-2">{cardContent.title}</p>
+      <p className="font-normal pb-4 w-[25rem]">{cardContent.content}</p>
     </div>
   );
 };
--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Card from '../components/detail-card/Card';
 import CommentList from '../components/comment/CommentList';
+import axios from 'axios';
 
 const Detail = () => {
+  const param = useParams();
+
+  const fetchData = async () => {
+    const { data } = await axios.get(`http://localhost:3001/post/${param.id}`);
+    console.log('data', data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="flex">
-      <Card />
+      <Card cardId={param.id} />
       <CommentList />
     </div>
   );
