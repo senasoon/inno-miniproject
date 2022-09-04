e870ee67fe271e1b17465dd85a9f790080b7d6ba
heyley <hyelimiam@gmail.com>
2022-09-05 04:03:19 +0900

    feat:json server cookie

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Detail from './views/Detail';
 import Login from './views/Login';
 import Register from './views/Register';
+import Main from './views/Main';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/detail" element={<Detail />} />
-        <Route path="/signin" element={<Register />} />
+        <Route path="/signup" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -2,9 +2,8 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = () => {
-
   return (
-    <div className='mx-24 pt-20 flex flex-row  grid grid-cols-4 gap-y-6 justify-items-center'>
+    <div className="mx-24 pt-20 flex flex-row  grid grid-cols-4 gap-y-6 justify-items-center">
       <Card />
       <Card />
       <Card />
