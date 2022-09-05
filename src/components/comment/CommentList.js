126fa5e900cc0a97c3cee31732b5dfbcb246feae
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-09-05 09:07:52 +0900

    feat: comment 조회 진행중

--- src/components/comment/CommentList.js
+++ src/components/comment/CommentList.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import CommentInput from './CommentInput';
-import Comment from './Comment';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { FaPencilAlt, FaTrash } from 'react-icons/fa';
 
 const CommentList = () => {
+  const [getComment, setGetComment] = useState('');
+  console.log(getComment[0]);
+
+  const param = useParams();
+
+  const fetchComments = async () => {
+    const { data } = await axios.get(
+      `http://localhost:3001/comment/?postId=${param.id}`,
+    );
+    setGetComment(data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
   return (
     <div>
-      <div className="card w-[34rem] h-[32rem]">
-        <CommentInput />
-        <Comment />
+      <div className="flex justify-between mt-2">
+        <div className="flex">
+          <p className="font-semibold pr-2">닉네임</p>
+          <p className="font-normal">여기에 댓글이 들어갑니다.</p>
+        </div>
+        <div>
+          <button className="mr-2">
+            <FaPencilAlt />
+          </button>
+          <button>
+            <FaTrash />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,25 +1,24 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+// import { useParams } from 'react-router-dom';
 import Card from '../components/detail-card/Card';
-import CommentList from '../components/comment/CommentList';
-import axios from 'axios';
+import Comment from '../components/comment/Comment';
+// import axios from 'axios';
 
 const Detail = () => {
-  const param = useParams();
+  // const param = useParams();
 
-  const fetchData = async () => {
-    const { data } = await axios.get(`http://localhost:3001/post/${param.id}`);
-    console.log('data', data);
-  };
+  // const fetchData = async () => {
+  //   await axios.get(`http://localhost:3001/post/${param.id}`);
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return (
     <div className="flex">
-      <Card cardId={param.id} />
-      <CommentList />
+      <Card />
+      <Comment />
     </div>
   );
 };
