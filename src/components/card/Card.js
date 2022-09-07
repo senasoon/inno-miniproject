213076c5e65f46e163486da45b32defe4900fcde
seeunpark <senasoon0@gmail.com>
2022-09-07 17:08:40 +0900

    feat: main GET, add POST

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,24 +4,27 @@ import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 useNavigate;
 
-const Card = ({ id, title, imgUrl }) => {
+const Card = ({ postId, title, imgUrl }) => {
   Card.propTypes = {
     title: PropTypes.node.isRequired,
     imgUrl: PropTypes.node.isRequired,
-    id: PropTypes.node.isRequired,
+    postId: PropTypes.node.isRequired,
   };
 
   const navigate = useNavigate();
   return (
     <div className="bg-white w-48 h-56 shadow-md">
       <img
-        onClick={() => navigate(`/detail/${id}`)}
+        onClick={() => navigate(`/detail/${postId}`)}
         src={imgUrl}
         className="mt-4 w-40 h-40 m-auto bg-cover cursor-pointer"
       />
       <div className="flex justify-between mx-4 h-12 items-center">
         <p className="text-xs">{title}</p>
-        <button onClick={() => navigate(`/detail/${id}`)} className="text-sm">
+        <button
+          onClick={() => navigate(`/detail/${postId}`)}
+          className="text-sm"
+        >
           <VscAdd />
         </button>
       </div>
